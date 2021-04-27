372204e76e41a120f1aaca6fb3b5bf722f971d5c
Cam Knight <cameron.knight@capgemini.com>
2021-04-27 15:10:46 +0100

    Start of Game

---
+++ Assets/Scripts/Picture.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picture : MonoBehaviour
+{
+
+    private Material _firstMaterial;
+    private Material _secondMaterial;
+
+    private Quaternion _currentRotation;
+
+    [HideInInspector] public bool Revealed = false;
+    private PictureManager _pictureManager;
+    private bool _clicked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Revealed = false;
+        _clicked = false;
+        _pictureManager = GameObject.Find("[PictureManager]").GetComponent<PictureManager>();
+        _currentRotation = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (_clicked == false)
+        {
+            _pictureManager.CurrentPuzzleState = PictureManager.PuzzleState.PuzzleRotating;
+            StartCoroutine(LoopRotation(45, false));
+            _clicked = true;
+        }
+    }
+
+    public void FlipBack()
+    {
+        if(gameObject.activeSelf)
+        {
+            _pictureManager.CurrentPuzzleState = PictureManager.PuzzleState.PuzzleRotating;
+            Revealed = false;
+            StartCoroutine(LoopRotation(45, true));
+
+        }
+    }
+
+    IEnumerator LoopRotation(float angle, bool FirstMat)
+    {
+        var rot = 0f;
+        const float dir = 1f;
+        const float rotSpeed = 180.0f;
+        const float rotSpeed1 = 90.0f;
+        var startAngle = angle;
+        var assigned = false;
+
+        if(FirstMat)
+        {
+            while (rot < angle)
+            {
+                var step = Time.deltaTime * rotSpeed1;
+                gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 2, 0) * step * dir);
+                if(rot >= (startAngle - 2) && assigned == false)
+                {
+                    ApplyFirstMaterial();
+                    assigned = true;
+                }
+
+                rot += (1 * step * dir);
+                yield return null;
+            }
+        }
+        else
+        {
+            while (angle > 0)
+            {
+                float step = Time.deltaTime * rotSpeed;
+                gameObject.GetComponent<Transform>().Rotate(new Vector3(0, 2, 0) * step * dir);
+                angle -= (1 * step * dir);
+                yield return null;
+            }
+        }
+
+        gameObject.GetComponent<Transform>().rotation = _currentRotation;
+
+        if(!FirstMat)
+        {
+            Revealed = true;
+            ApplySecondMaterial();
+            _pictureManager.CheckPicture();
+        }
+        else
+        {
+            _pictureManager.PuzzleRevealedNumber = PictureManager.RevealedState.NoRevealed;
+            _pictureManager.CurrentPuzzleState = PictureManager.PuzzleState.CanRotate;
+        }
+
+        _clicked = false;
+    }
+
+    public void SetFirstMaterial(Material mat, string teaxturePath)
+    {
+        _firstMaterial = mat;
+        _firstMaterial.mainTexture = Resources.Load(teaxturePath, typeof(Texture2D)) as Texture2D;
+
+    }
+
+    public void SetSecondMaterial(Material mat, string teaxturePath)
+    {
+        _secondMaterial = mat;
+        _secondMaterial.mainTexture = Resources.Load(teaxturePath, typeof(Texture2D)) as Texture2D;
+
+    }
+
+    public void ApplyFirstMaterial()
+    {
+        gameObject.GetComponent<Renderer>().material = _firstMaterial;
+    }
+
+    public void ApplySecondMaterial()
+    {
+        gameObject.GetComponent<Renderer>().material = _secondMaterial;
+    }
+}
---
+++ Assets/Scripts/PictureManager.cs
@@ -0,0 +1,256 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureManager : MonoBehaviour
+{
+
+    public Picture PicturePrefab;
+    public Transform PicSpawnPosition;
+    public Vector2 StartPosition = new Vector2(-2.15f, 3.62f);
+
+    public enum GameState { NoAction, MovingOnPositions, DeletingPuzzles, FlipBack, Checking, GameEnd };
+
+    public enum PuzzleState { PuzzleRotating, CanRotate };
+
+    public enum RevealedState { NoRevealed, OneRevealed, TwoRevealed };
+
+    [HideInInspector]
+    public GameState CurrentGameState;
+    [HideInInspector]
+    public PuzzleState CurrentPuzzleState;
+    [HideInInspector]
+    public RevealedState PuzzleRevealedNumber;
+
+    [HideInInspector]
+    public List<Picture> PictureList;
+
+    private Vector2 _offset = new Vector2(1.5f, 1.52f);
+    private Vector2 _offsetFor15Pairs = new Vector2(1.08f, 1.22f);
+    private Vector2 _offsetFor20Pairs = new Vector2(1.08f, 1.0f);
+    private Vector3 _newScaleDown = new Vector3(0.9f, 0.9f, 0.001f);
+
+
+    private List<Material> _materialList = new List<Material>();
+    private List<string> _texturePathList = new List<string>();
+    private Material _firstMaterial;
+    private string _firstTexturePath;
+
+    private int _firstRevealedPic;
+    private int _secondRevealedPic;
+    private int _revealedPicNumber = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentGameState = GameState.NoAction;
+        CurrentPuzzleState = PuzzleState.CanRotate;
+        PuzzleRevealedNumber = RevealedState.NoRevealed;
+        _revealedPicNumber = 0;
+        _firstRevealedPic = -1;
+        _secondRevealedPic = -1;
+
+        LoadMaterials();
+
+
+        if(GameSettings.Instance.GetPairNumber() == GameSettings.EPairNumber.E10Pairs)
+        {
+            CurrentGameState = GameState.MovingOnPositions;
+            SpawnPictureMesh(4, 5, StartPosition, _offset, false);
+            MovePicture(4, 5, StartPosition, _offset);
+        }
+        else if (GameSettings.Instance.GetPairNumber() == GameSettings.EPairNumber.E15Pairs)
+        {
+            CurrentGameState = GameState.MovingOnPositions;
+            SpawnPictureMesh(5, 6, StartPosition, _offset, false);
+            MovePicture(5, 6, StartPosition, _offsetFor15Pairs);
+        }
+        if (GameSettings.Instance.GetPairNumber() == GameSettings.EPairNumber.E20Pairs)
+        {
+            CurrentGameState = GameState.MovingOnPositions;
+            SpawnPictureMesh(5, 8, StartPosition, _offset, true);
+            MovePicture(5, 8, StartPosition, _offsetFor20Pairs);
+        }
+
+    }
+
+    public void CheckPicture()
+    {
+        CurrentGameState = GameState.Checking;
+        _revealedPicNumber = 0;
+
+        for(int id = 0; id < PictureList.Count; id++)
+        {
+            if (PictureList[id].Revealed && _revealedPicNumber < 2)
+            {
+                if(_revealedPicNumber == 0)
+                {
+                    _firstRevealedPic = id;
+                    _revealedPicNumber++;
+                }
+                else if (_revealedPicNumber == 1)
+                {
+                    _secondRevealedPic = id;
+                    _revealedPicNumber++;
+                }
+            }
+        }
+
+        if (_revealedPicNumber == 2)
+        {
+            CurrentGameState = GameState.FlipBack;
+        }
+
+        CurrentPuzzleState = PictureManager.PuzzleState.CanRotate;
+
+        if(CurrentGameState == GameState.Checking)
+        {
+            CurrentGameState = GameState.NoAction;
+        }
+    }
+
+    private void FlipBack()
+    {
+        System.Threading.Thread.Sleep(500); //test remove
+
+        PictureList[_firstRevealedPic].FlipBack();
+        PictureList[_secondRevealedPic].FlipBack();
+
+        PictureList[_firstRevealedPic].Revealed = false;
+        PictureList[_secondRevealedPic].Revealed = false;
+
+        PuzzleRevealedNumber = RevealedState.NoRevealed;
+        CurrentGameState = GameState.NoAction;
+    }
+
+    private void LoadMaterials()
+    {
+        var materialFilePAth = GameSettings.Instance.GetMaterialDirectoryName();
+        var textureFilePath = GameSettings.Instance.GetPuzzleCategoryTextureDirectoryName();
+        var pairNumber = (int)GameSettings.Instance.GetPairNumber();
+        const string matBaseName = "Pic";
+        var firstMaterialName = "Back";
+
+        for(var index = 1; index <= pairNumber; index++)
+        {
+            var currentFilePath = materialFilePAth + matBaseName + index;
+            Material mat = Resources.Load(currentFilePath, typeof(Material)) as Material;
+            _materialList.Add(mat);
+
+            var currentTextureFilePath = textureFilePath + matBaseName + index;
+            _texturePathList.Add(currentTextureFilePath);
+        }
+
+        _firstTexturePath = textureFilePath + firstMaterialName;
+        _firstMaterial = Resources.Load(materialFilePAth + firstMaterialName, typeof(Material)) as Material;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentGameState == GameState.FlipBack)
+        {
+            if(CurrentPuzzleState == PuzzleState.CanRotate)
+            {
+                FlipBack();
+            }
+        }
+    }
+
+
+    private void SpawnPictureMesh(int rows, int columns, Vector2 Pos, Vector2 offset, bool scaleDown)
+    {
+        for(int col = 0; col < columns; col++)
+        {
+            for(int row = 0; row < rows; row++)
+            {
+                var tempPicture = (Picture)Instantiate(PicturePrefab, PicSpawnPosition.position, PicturePrefab.transform.rotation);
+
+                if(scaleDown)
+                {
+                    tempPicture.transform.localScale = _newScaleDown;
+                }
+
+                tempPicture.name = tempPicture.name + 'c' + col + 'r' + row;
+                PictureList.Add(tempPicture);
+            }
+        }
+
+        ApplyTextures();
+    }
+
+    public void ApplyTextures()
+    {
+        var rndMatIndex = Random.Range(0, _materialList.Count);
+        var AppliedTimes = new int[_materialList.Count];
+
+        for(int i = 0; i < _materialList.Count; i++)
+        {
+            AppliedTimes[i] = 0;
+        }
+
+        foreach(var o in PictureList)
+        {
+            var randPrevious = rndMatIndex;
+            var counter = 0;
+            var forceMat = false;
+
+            while(AppliedTimes[rndMatIndex] >= 2 || ((randPrevious == rndMatIndex) && !forceMat) )
+            {
+                rndMatIndex = Random.Range(0, _materialList.Count);
+                counter++;
+                if(counter > 100)
+                {
+                    for(var j= 0; j < _materialList.Count; j++)
+                    {
+                        if(AppliedTimes[j] < 2)
+                        {
+                            rndMatIndex = j;
+                            forceMat = true;
+                        }
+                    }
+
+                    if (forceMat == false)
+                        return;
+                }
+            }
+
+            o.SetFirstMaterial(_firstMaterial, _firstTexturePath);
+            o.ApplyFirstMaterial();
+            o.SetSecondMaterial(_materialList[rndMatIndex], _texturePathList[rndMatIndex]);
+
+            //o.ApplySecondMaterial(); //Test to show flipped cards revealing the numbers
+
+            AppliedTimes[rndMatIndex] += 1;
+            forceMat = false;
+        }
+
+    }
+
+    private void MovePicture(int rows, int columns, Vector2 pos, Vector2 offset)
+    {
+        var index = 0;
+        for (var col = 0; col < columns; col++)
+        {
+            for(int row = 0; row < rows; row++)
+            {
+                var targetPosition = new Vector3((pos.x + (offset.x * row)), (pos.y - (offset.y * col)), 0.0f);
+                StartCoroutine(MoveToPosition(targetPosition, PictureList[index]));
+                index++;
+            }
+        }
+    }
+
+    private IEnumerator MoveToPosition(Vector3 target, Picture obj)
+    {
+        var randomDis = 7;
+
+        while(obj.transform.position != target)
+        {
+            obj.transform.position = Vector3.MoveTowards(obj.transform.position, target, randomDis * Time.deltaTime);
+            yield return 0;
+        }
+    }
+}
